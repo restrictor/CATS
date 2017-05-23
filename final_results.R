890801cebe38286b8df1cf936cbae990d2e5601e
restrictor <d2.bijkerk@student.vu.nl>
2017-05-23 09:23:48 +0200

    final

--- final_results.R
+++ final_results.R
@@ -1,8 +1,8 @@
-nb = c(73,78,72,72,72,81)
-svm = c(86,82,86,87,86,84)
-nn = c(88,84,89,89,89,86)
-mr = c(81,84,86,86,86,87)
-rf = c(86,84,85,85,81,87)
+nb = c(73,78,72,72,72,81,76)
+svm = c(86,82,86,87,86,84,82)
+nn = c(88,84,89,89,89,86,83)
+mr = c(81,84,86,86,86,87,87)
+rf = c(86,84,85,85,81,87,81)
 
 SIMPLE = data.frame(nb,svm,nn,mr,rf)
 SIMPLE_mean = apply(SIMPLE, 2, mean)
@@ -10,11 +10,11 @@ SIMPLE_sd = apply(SIMPLE, 2, sd)
 SIMPLE_mean
 SIMPLE_sd
 
-RFSVM = c(86,88,90,86,88,85,89)
-RF2SVM = c(93,90,93,93,93,92,93)
-NNET2SVM = c(91,91,93,92,92,91,91)
-SVM2SVM = c(91,90,93,92,92,91,91)
-SVM2SVMRF = c(91,87,91,91,91,90,88)
+RFSVM = c(86,88,90,86,88,85,89,86)
+RF2SVM = c(93,90,93,93,93,92,93,92)
+NNET2SVM = c(91,91,93,92,92,91,91,91)
+SVM2SVM = c(91,90,93,92,92,91,91,91)
+SVM2SVMRF = c(91,87,91,91,91,90,88,88)
 
 EASY = data.frame(RFSVM,RF2SVM,NNET2SVM,SVM2SVM,SVM2SVMRF)
 EASY_mean = apply(EASY, 2, mean)
